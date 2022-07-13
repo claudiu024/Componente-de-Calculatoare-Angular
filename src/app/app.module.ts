c0cc55b8cd3027f43f115f8c7c6ecd7cb268092c
claudiu <claudiu.andronache@student.unitbv.ro>
2022-07-13 12:50:07 +0300

    adaugare login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import {MatInputModule} from '@angular/material/input'
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,17 @@
+import { Product } from '../product/product.component';
+export class ProductService {
+  products: Product[] = [
+    {
+      id: 0,
+      title: 'title1',
+      description: '',
+      price: 23,
+    },
+    {
+      id: 1,
+      title: 'title2',
+      description: '',
+      price: 23,
+    },
+  ];
+}
