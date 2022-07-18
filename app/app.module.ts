0107f0bde83fba37cb1f0bf82e5d863a09aa49d9
raresang123 <108883005+raresang123@users.noreply.github.com>
2022-07-18 11:57:43 +0300

    Add files via upload
    add component coscumparaturi

---
+++ app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule, routingComponent } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import {MatInputModule} from '@angular/material/input'
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatCardModule} from '@angular/material/card';
+
+//import { ProductComponent } from './product/product.component';
+
+import { FormsModule } from '@angular/forms';
+import { CoscumparaturiComponent } from './coscumparaturi/coscumparaturi.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    routingComponent,
+    CoscumparaturiComponent,
+    
+    //ProductComponent,
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    FormsModule,
+    MatCardModule
+   
+    
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app/coscumparaturi/coscumparaturi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coscumparaturi',
+  templateUrl: './coscumparaturi.component.html',
+  styleUrls: ['./coscumparaturi.component.css']
+})
+export class CoscumparaturiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
